9fc3424ed20e9f5d990cab584996b8366518bd21
emmathedeveloper2 <emmadavid063@gmail.com>
2024-10-22 12:41:20 +0100

    Minor fixes

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -3,6 +3,10 @@ import Nav from '../components/nav'
 import Footer from '../components/footer'
 
 export const Route = createRootRoute({
+  meta: () => [
+    {title: 'Bump'},
+    {description: 'Get homes and condos suited to your needs'},
+  ],
   component: () => (
     <>
       <Nav />
